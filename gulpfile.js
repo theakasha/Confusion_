b782beb4e76657676f60cbb59f60bfe0c61c5422
theakasha <akash23aryan@gmail.com>
2020-06-19 15:38:07 +0530

    final

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync');
+    del = require('del'),
+    imagemin = require('gulp-imagemin'),
+    gulp.task('sass', function () {
+        return gulp.src('./css/*.scss')
+          .pipe(sass().on('error', sass.logError))
+          .pipe(gulp.dest('./css'));
+      });
+      
+      gulp.task('sass:watch', function () {
+        gulp.watch('./css/*.scss', ['sass']);
+      });
+      
+      gulp.task('browser-sync', function () {
+         var files = [
+            './*.html',
+            './css/*.css',
+            './img/*.{png,jpg,gif}',
+            './js/*.js'
+         ];
+      
+         browserSync.init(files, {
+            server: {
+               baseDir: "./"
+            }
+         });
+      
+      });
+      
+      // Default task
+      gulp.task('default', ['browser-sync'], function() {
+          gulp.start('sass:watch');
+      });
+      gulp.task('clean', function() {
+        return del(['dist']);
+    });
+    
+    gulp.task('copyfonts', function() {
+       gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+       .pipe(gulp.dest('./dist/fonts'));
+    });
+
+    gulp.task('imagemin', function() {
+        return gulp.src('img/*.{png,jpg,gif}')
+          .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+          .pipe(gulp.dest('dist/img'));
+      });
+      gulp.task('build',['clean'], function() {
+        gulp.start('copyfonts','imagemin','usemin');
+    });
+      
